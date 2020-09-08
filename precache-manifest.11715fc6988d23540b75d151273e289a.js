bb9b864b07ef72e76a358d92167051bc5f179c69
wassim miladi <wassim22m@gmail.com>
2020-09-08 21:52:47 +0200

    Updates

---
+++ precache-manifest.11715fc6988d23540b75d151273e289a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5a01797082cf065cc3a3eb868899e4",
+    "url": "/algo-fun/index.html"
+  },
+  {
+    "revision": "d71b0e7d80432ee5a196",
+    "url": "/algo-fun/static/css/2.5455c339.chunk.css"
+  },
+  {
+    "revision": "7e36618caab6c7343b6e",
+    "url": "/algo-fun/static/css/main.028f193b.chunk.css"
+  },
+  {
+    "revision": "d71b0e7d80432ee5a196",
+    "url": "/algo-fun/static/js/2.46fd6be5.chunk.js"
+  },
+  {
+    "revision": "12e41d8f132b1d17c29409b17e6b8cc5",
+    "url": "/algo-fun/static/js/2.46fd6be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e36618caab6c7343b6e",
+    "url": "/algo-fun/static/js/main.1e8b101c.chunk.js"
+  },
+  {
+    "revision": "948c52fd9934fdd3fad1",
+    "url": "/algo-fun/static/js/runtime-main.08d5f547.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/algo-fun/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/algo-fun/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/algo-fun/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/algo-fun/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/algo-fun/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/algo-fun/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/algo-fun/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/algo-fun/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/algo-fun/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/algo-fun/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/algo-fun/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/algo-fun/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/algo-fun/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/algo-fun/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/algo-fun/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/algo-fun/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
